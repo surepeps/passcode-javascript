37c1872c07f862b1336d21c5fbe18cf109a27bb0
surecoder <gatukurh1@gmail.com>
2023-08-12 12:19:58 +0100

    display clear button

--- js/passcode.js
+++ js/passcode.js
@@ -12,8 +12,18 @@ let numbersBtnLength = numbersBtn.length;
 // Create a for loop
 for (let i = 0; i < numbersBtnLength; i++){
     numbersBtn[i].addEventListener("click", function() {
-        codeArray.push(this.textContent);
-        console.log(codeArray);
+
+        // check if the length of the array is 4
+        if (codeArray.length < 4) {
+            codeArray.push(this.textContent);
+            console.log(codeArray);
+        }
+
+        // activate the delete icon when the array lenght is greater or equal to 1
+        if (codeArray.length > 0) {
+            deleteNum.style.visibility = "visible";
+        }
+    
     })
 }
 
