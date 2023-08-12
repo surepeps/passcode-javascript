2af363f2872f4babfdf3b69cd5b4915ba36cd363
Ebere Onuobia <cynthiaonuobia@gmail.com>
2023-08-12 15:13:36 +0100

    finish line

--- js/passcode.js
+++ js/passcode.js
@@ -6,6 +6,10 @@ let deleteNum = document.getElementById("buttonClear");
 // Empty array for code collection
 let codeArray = [];
 
+// Set the Original password
+let realPass = "4608";
+
+
 // Listen to each of the buttons
 let numbersBtnLength = numbersBtn.length;
 
@@ -17,7 +21,22 @@ for (let i = 0; i < numbersBtnLength; i++){
         if (codeArray.length < 4) {
             codeArray.push(this.textContent);
             // display the numbers
-            displayCode(codeArray)
+            let userCode = displayCode(codeArray)
+
+            if (userCode.length == 4) {
+                if (userCode  === realPass) {
+                    displayTag.style.border = "3px solid green"
+
+                    setTimeout(function(){
+                        console.log("success")
+                    },1000)
+                } else {
+                    displayTag.style.border = "3px solid red"
+
+                }
+
+            }
+
         }
 
         // activate the delete icon when the array lenght is greater or equal to 1
@@ -32,6 +51,7 @@ for (let i = 0; i < numbersBtnLength; i++){
 function displayCode(numArray) {
     let newNums = numArray.join("");
     displayTag.textContent = newNums;
+    return newNums;
 }
 
 // The delete action
@@ -41,6 +61,9 @@ deleteNum.addEventListener("click",function() {
         codeArray.pop()
         // This is displaying the current array once the delete button is clicked
         displayCode(codeArray)
+
+        // Remove the Red border Alert after delete button is clicked
+        displayTag.style.border = "none";
     }
     // This checks if there is no value in the array and then hides the delete button
     if(!codeArray.length) {
